11360e17a6c8ec5b404507c6cac8a9b863958768
hirachan <yo@hirano.cc>
2023-06-18 18:18:17 +0900

    add 0-2

--- travels/add_img.sh
+++ travels/add_img.sh
@@ -5,5 +5,5 @@ git add $(grep '\.\./img/' content/bicycle/BPB2016/*/*.md | sed 's|.*/img/\([^)]
 git add $(grep '\.\./img/' content/bicycle/bkk-laos/*/*.md | grep -v http | sed 's|.*/img/\([^)]*\))|content/bicycle/bkk-laos/img/\1|')
 git add $(grep '\./img/' content/bicycle/brm911-2021/*.md | grep -v http | sed 's|.*/img/\([^)]*\))|content/bicycle/brm911-2021/img/\1|')
 
-git add $(grep '\./img/' content/bicycle/korea2030/*.md | grep -v http | sed 's|.*/img/\([^)]*\))|content/bicycle/korea2030/img/\1|')
-# git add $(grep '\.\./img/' content/bicycle/korea2030/*/*.md | grep -v http | sed 's|.*/img/\([^)]*\))|content/bicycle/korea2030/img/\1|')
+# git add $(grep '\./img/' content/bicycle/korea2030/*.md | grep -v http | sed 's|.*/img/\([^)]*\))|content/bicycle/korea2030/img/\1|')
+git add $(grep '\.\./img/' content/bicycle/korea2030/*/*.md | grep -v http | sed 's|.*/img/\([^)]*\))|content/bicycle/korea2030/img/\1|')
