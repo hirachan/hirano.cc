01c97ef77cf0c3ba2cf1c445599262e6f7dc1822
hirachan <yo@hirano.cc>
2025-10-05 11:34:34 +0900

    add silkroad

--- travels/add_img.sh
+++ travels/add_img.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-git add $(grep '\.\./img/' content/bicycle/isan2020/*/*.md | sed 's|.*/img/\([^)]*\))|content/bicycle/isan2020/img/\1|')
-git add $(grep '\.\./img/' content/bicycle/BPB2016/*/*.md | sed 's|.*/img/\([^)]*\))|content/bicycle/BPB2016/img/\1|')
-git add $(grep '\.\./img/' content/bicycle/bkk-laos/*/*.md | grep -v http | sed 's|.*/img/\([^)]*\))|content/bicycle/bkk-laos/img/\1|')
-git add $(grep '\./img/' content/bicycle/brm911-2021/*.md | grep -v http | sed 's|.*/img/\([^)]*\))|content/bicycle/brm911-2021/img/\1|')
+# git add $(grep '\.\./img/' content/bicycle/isan2020/*/*.md | sed 's|.*/img/\([^)]*\))|content/bicycle/isan2020/img/\1|')
+# git add $(grep '\.\./img/' content/bicycle/BPB2016/*/*.md | sed 's|.*/img/\([^)]*\))|content/bicycle/BPB2016/img/\1|')
+# git add $(grep '\.\./img/' content/bicycle/bkk-laos/*/*.md | grep -v http | sed 's|.*/img/\([^)]*\))|content/bicycle/bkk-laos/img/\1|')
+# git add $(grep '\./img/' content/bicycle/brm911-2021/*.md | grep -v http | sed 's|.*/img/\([^)]*\))|content/bicycle/brm911-2021/img/\1|')
 
 # git add $(grep '\./img/' content/bicycle/korea2030/*.md | grep -v http | sed 's|.*/img/\([^)]*\))|content/bicycle/korea2030/img/\1|')
-git add $(grep '\.\./img/' content/bicycle/korea2030/*/*.md | grep -v http | sed 's|.*/img/\([^)]*\))|content/bicycle/korea2030/img/\1|')
+# git add $(grep '\.\./img/' content/bicycle/korea2030/*/*.md | grep -v http | sed 's|.*/img/\([^)]*\))|content/bicycle/korea2030/img/\1|')
+git add $(grep '\.\./img/' content/bicycle/silkroad2000/*/*.md | grep -v http | sed 's|.*/img/\([^)]*\))|content/bicycle/silkroad2000/img/\1|')
